2a58498e433e6f13060587c91e2a7810d34ee948
Тетяна Невідома <nevidomatanya89@gmail.com>
2023-04-27 16:18:37 +0300

    upd

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -36,8 +36,6 @@ export default function MovieDetails() {
 
   const { genres, overview, vote_average, title, poster_path } = movie;
   const genresList = genres?.map(genre => genre.name).join(', ');
-  const userScore = Math.round(vote_average * 10);
-  const MovieInfo = { title, poster_path, userScore, overview, genresList };
 
   return (
     <main className="containerMain">
@@ -49,20 +47,17 @@ export default function MovieDetails() {
       >
         Go back
       </button>
-      {movie && <MovieInfo movie={movie} />}
-      {movie && (
-        <div className="wrapper">
-          <img width="250" src={getPoster(poster_path)} alt={title} />
-          <div className="details">
-            <h2>{title}</h2>
-            <p>User Score: {userScore}</p>
-            <h3>Overview</h3>
-            <p>{overview}</p>
-            <h4>Genres</h4>
-            <p>{genresList}</p>
-          </div>
+      <div className="wrapper">
+        <img width="250" src={getPoster(poster_path)} alt={title} />
+        <div className="details">
+          <h2>{title}</h2>
+          <p>User Score: {(vote_average * 10).toFixed()} %</p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h4>Genres</h4>
+          <p>{genresList}</p>
         </div>
-      )}
+      </div>
       <h3>Additional information</h3>
       <div className="wrapper">
         <NavLink className="navLink" to="cast">
